3ca137f3cf185e373964b9b2e85ed4cc9f8eb04c
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-19 20:06:24 +0100

    added mote business logic like number format exception

--- src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -19,10 +19,21 @@ public class ValidateISBN {
 	 */
 
 	public boolean checkISBN(String isbn) {
-		if (isbn == "0140449116") {
+		if (isbn.length() != 10) throw new NumberFormatException("ISBN number must be 10 digit");
+			
+		
+		int total = 0;
+		for (int i = 0; i < 10; i++) {
+			if (!Character.isDigit(isbn.charAt(i))) throw new NumberFormatException("ISBN number can only contain numeric digit"); 
+			total += isbn.charAt(i) *(10 - i);
+		}
+		
+		if (total % 11 == 0 ){
 			return true;
 		}
 		return false;
+		
+		 
 	}
 
 }
