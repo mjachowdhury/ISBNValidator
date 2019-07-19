5053321d87c6ed268f68e69cad23309a9cb9796a
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-19 19:15:36 +0100

    First commit

---
+++ src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.virtualpairprogrammers.isbntools;
+
+/**
+ * @author malom
+ *
+ */
+public class ValidateISBN {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
